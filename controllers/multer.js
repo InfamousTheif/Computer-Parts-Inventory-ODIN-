e24d7cfcc0e5077f6edf61d78825a8bf1bd70894
inquntum <inquntum@gmail.com>
2026-03-01 11:58:36 +0800

    added a file to handle multer

---
+++ controllers/multer.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '../Inventory manager/user-images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '--' + file.originalname);
+  }
+});
+
+const upload = multer({storage : fileStorageEngine});
+
+export { upload }
