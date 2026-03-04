9b6e358960dee2c88ff18577146c28e8655ca499
inquntum <inquntum@gmail.com>
2026-03-04 19:05:26 +0800

    added a filter that rejects all incorrect mime types and extenstions and added a filesize limit (1MB)

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import path from 'path';
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -9,6 +10,24 @@ const fileStorageEngine = multer.diskStorage({
   }
 });
 
-const upload = multer({storage : fileStorageEngine});
+function fileFilter(req, file, cb) {
+  const allowedExt = [".png", ".jpg", ".jpeg", ".webp"];
+  const allowedMime = ["image/png", "image/jpg", "image/jpeg", "image/webp"];
+
+  const ext = path.extname(file.originalname).toLowerCase();
+
+  if (!allowedExt.includes(ext) || !allowedMime.includes(file.mimetype)) {
+    return cb(new Error("Only png, jpg, jpeg, and webp files are allowed"));
+  }
+
+  cb(null, true);
+}
+
+const upload = multer({storage : fileStorageEngine,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1024 * 1024, // 1MB
+  }
+});
 
 export { upload }
